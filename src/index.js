705e3204c516f55fa27e595fb91cbc44c9bd6c0f
EnzoEsper <enzoesper@hotmail.com>
2019-05-18 18:07:24 -0300

    import SeasonDisplay component and passig to it the latitud parameter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
 
 // Componente basado en clase
 class App extends React.Component {
   
   state = {lat: null, errorMessage: ''}
-  
+
   componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({lat: position.coords.latitude}) ,
@@ -17,7 +17,7 @@ class App extends React.Component {
   // React requiere que definamos render()
   render() {
     if (this.state.lat && !this.state.errorMessage) {
-      return <div>Latitude:{this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat}/>
     }
 
     if (!this.state.lat && this.state.errorMessage) {
