de69173507b720a7d5cc4e9e215fdf1856d54fca
EnzoEsper <enzoesper@hotmail.com>
2019-05-19 04:52:09 -0300

    spinner animation for loading the geolocation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
-
+import Spinner from './Spinner';
 // Componente basado en clase
 class App extends React.Component {
   
@@ -24,7 +24,7 @@ class App extends React.Component {
       return <div>Error:{this.state.errorMessage}</div>
     }
 
-    return <div>Loading...</div>
+    return <Spinner /> ;
   }
 }
 
